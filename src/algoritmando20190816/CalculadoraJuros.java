43fc48f78f0a47f250eb7bc2516a79dbe61ad3ca
JC Bombardelli <jc.bombardelli@live.com>
2019-08-16 11:39:48 -0300

    aula 16 08 2019

---
+++ src/algoritmando20190816/CalculadoraJuros.java
@@ -0,0 +1,53 @@
+package algoritmando20190816;
+
+import java.util.Scanner;
+
+public class CalculadoraJuros {
+
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Inicie a calculadora \n 1 - Calculadora de Juros Simples \n 2 - Calculadora Juros Compostos");
+		System.out.print("Escolha uma opção: ");
+		
+		int opcao = Integer.parseInt(sc.nextLine());
+		
+		if (opcao == 1) {
+			
+			System.out.print("Digite a taxa de juros: ");
+			float taxaDeJuros = Float.parseFloat(sc.nextLine());
+			
+			//Alterar para ponto flutuante
+			System.out.print("Digite o capital: ");
+			int capital = Integer.parseInt(sc.nextLine());
+			
+			System.out.print("Digite o tempo (em dias): ");	
+			int tempo = Integer.parseInt(sc.nextLine());
+			
+			float jurosResultado = calculoJurosSimples(taxaDeJuros, capital, tempo);
+			
+			System.out.println("O total de juros em reais é de: R$"+jurosResultado);
+			System.out.println("O total de capital é de R$:"+ (jurosResultado + capital)  );
+				
+		}
+		else if (opcao == 2) {
+			
+		}
+	}
+	
+	public static float calculoJurosSimples(float taxaDeJuros, int capital, int tempo) {
+		float jurosResultado = capital * taxaDeJuros * tempo;
+		return jurosResultado ; 
+	}
+	
+	
+	//Implementar
+	public static float calculoJurosCompostos() {
+		
+		return 0;
+	}
+
+}
